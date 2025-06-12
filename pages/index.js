815dc4febfa8a7d253314489dc555d2cc6849a40
tmlidrsog <temelidrisog@gmail.com>
2025-06-13 01:31:28 +0300

    Sayfa yapısı eklendi: index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ color: 'white', background: '#111', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Altın Hesaplayıcı Web Uygulaması</h1>
+    </div>
+  );
+}
